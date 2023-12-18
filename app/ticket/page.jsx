8f6e706be66bd68acff9668960231d2b95926e60
ROHIT RANJAN SINGH <rohitranjan2702@gmail.com>
2023-12-19 01:58:12 +0530

    okok

--- app/ticket/page.jsx
+++ app/ticket/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 import useAuthentication from "@/utils/useAuth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const Ticket = () => {
   useAuthentication();
 
   const [data, setData] = useState(null);
+  const router = useRouter();
 
   const fetchData = async (email) => {
     const res = await fetch(
@@ -28,10 +30,65 @@ const Ticket = () => {
     setData(result?.data);
   };
 
+  const checkIn1 = async (email) => {
+    const res = await fetch("https://chemcon-backend.onrender.com/api/check1", {
+      method: "POST",
+      body: JSON.stringify({
+        data: {
+          email: email,
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await res.json();
+    console.log(result);
+    setData(result?.data);
+    router.refresh();
+  };
+
+  const checkIn2 = async (email) => {
+    const res = await fetch("https://chemcon-backend.onrender.com/api/check2", {
+      method: "POST",
+      body: JSON.stringify({
+        data: {
+          email: email,
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await res.json();
+    console.log(result);
+    setData(result?.data);
+    router.refresh();
+  };
+
+  const checkIn3 = async (email) => {
+    const res = await fetch("https://chemcon-backend.onrender.com/api/check3", {
+      method: "POST",
+      body: JSON.stringify({
+        data: {
+          email: email,
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await res.json();
+    console.log(result);
+    setData(result?.data);
+    router.refresh();
+  };
+
   useEffect(() => {
     const email = localStorage.getItem("userEmail");
     if (!email) {
       alert("Invalid QR Code");
+      router.push("/scan");
       return;
     }
     fetchData(email);
@@ -59,22 +116,22 @@ const Ticket = () => {
               Email: {data?.Email}
             </p>
             <p class="mb-3 font-normal text-gray-700 :text-gray-400">
-              Mobile: {data?.mobile}
+              Mobile: {data?.Mobile}
             </p>
-            <p class="mb-3 font-normal text-gray-700 :text-gray-400">
-              Org: {data?.org}
-            </p>
+
             <p class="mb-3 font-normal text-gray-700 :text-gray-400">
               Kit: {data?.Kit}
             </p>
             <p class="mb-3 font-normal text-gray-700 :text-gray-400">
               Category: {data?.Category}
             </p>
-            <div className="flex flex-row gap-3">
+            {/* TODO resolve chechIn */}
+            {/* <div className="flex flex-row gap-3">
               <button
                 href="#"
                 class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800"
                 disabled={data?.checkin1}
+                onClick={checkIn1}
               >
                 Day 1 CheckIn
                 <svg
@@ -97,6 +154,7 @@ const Ticket = () => {
                 href="#"
                 class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800"
                 disabled={data?.checkin2}
+                onClick={checkIn2}
               >
                 Day 2 CheckIn
                 <svg
@@ -119,6 +177,7 @@ const Ticket = () => {
                 href="#"
                 class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800"
                 disabled={data?.checkin3}
+                onClick={checkIn3}
               >
                 Day 3 CheckIn
                 <svg
@@ -137,7 +196,7 @@ const Ticket = () => {
                   />
                 </svg>
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
         <Link
