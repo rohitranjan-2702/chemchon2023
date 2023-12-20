45fa8f530948211d068821299d6edefbf12db247
ROHIT RANJAN SINGH <rohitranjan2702@gmail.com>
2023-12-21 00:23:43 +0530

    checkin

--- app/ticket/page.jsx
+++ app/ticket/page.jsx
@@ -125,31 +125,32 @@ const Ticket = () => {
             <p class="mb-3 font-normal text-gray-700 :text-gray-400">
               Category: {data?.Category}
             </p>
+            <button
+              href="#"
+              class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800"
+              disabled={data?.checkin1}
+              onClick={checkIn1}
+            >
+              Day 1 CheckIn
+              <svg
+                class="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </button>
             {/* TODO resolve chechIn */}
             {/* <div className="flex flex-row gap-3">
-              <button
-                href="#"
-                class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800"
-                disabled={data?.checkin1}
-                onClick={checkIn1}
-              >
-                Day 1 CheckIn
-                <svg
-                  class="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
+              
               <button
                 href="#"
                 class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800"
